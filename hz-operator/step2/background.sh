e129cb8d67721bb49546fe72cd5ca1865220eb66
aborshchuk <alexander.borschuk@hazelcast.com>
2024-09-24 23:57:50 +0200

    test license key

--- hz-operator/step2/background.sh
+++ hz-operator/step2/background.sh
@@ -3,4 +3,3 @@ sudo apt-get install -y whiptail
 cat << 'EOF' > password_prompt.sh
 LICENSE_KEY=$(whiptail --passwordbox "Please enter your License Key" 8 78 --title "License key" 3>&1 1>&2 2>&3)
 EOF
-sleep 2
--- hz-operator/step2/foreground.sh
+++ hz-operator/step2/foreground.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sleep 2
 source password_prompt.sh
