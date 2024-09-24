f2a3605388d506aab5b3cffff90218ba0b801c38
aborshchuk <alexander.borschuk@hazelcast.com>
2024-09-25 01:18:09 +0200

    test license key

--- hz-operator/intro.md
+++ hz-operator/intro.md
@@ -1,3 +1,3 @@
 <img src="./assets/hazelcast-logo.png" width="300px">
 
-## In this tutorial, you’ll connect to a Hazelcast cluster running in Kubernetes from outside the Kubernetes environment.
+### <span style="color:#10A4B3;">In this tutorial, you’ll connect to a Hazelcast cluster running in Kubernetes from outside the Kubernetes environment</span>
--- hz-operator/step1/background.sh
+++ hz-operator/step1/background.sh
@@ -1,12 +1,8 @@
 sudo apt-get install -y whiptail
 cat << 'EOF' > password_prompt.sh
 while true; do
-    # Prompt the user for the license key using whiptail
     LICENSE_KEY=$(whiptail --passwordbox "Please enter your License Key" 8 78 --title "License key" --nocancel 3>&1 1>&2 2>&3)
-
-    # Check if the LICENSE_KEY is not empty
     if [ -n "$LICENSE_KEY" ]; then
-        # If valid, echo the LICENSE_KEY and break the loop
         echo "$LICENSE_KEY"
         break
     fi
