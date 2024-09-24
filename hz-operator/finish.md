03e77a1ded6b2a1001439ef7f3594bbec429ae93
aborshchuk <alexander.borschuk@hazelcast.com>
2024-09-25 01:32:07 +0200

    test license key

--- hz-operator/finish.md
+++ hz-operator/finish.md
@@ -2,4 +2,4 @@
 
 You have installed and configured Hazelcast Operator.
 
-In the [next tutorial](https://docs.hazelcast.com/tutorials/hazelcast-platform-operator-external-backup-restore), you earn how to back up data in Hazelcast maps to cloud storage and restore a cluster from that backup data.
+In the [next tutorial](https://docs.hazelcast.com/tutorials/hazelcast-platform-operator-external-backup-restore), you learn how to back up data in Hazelcast maps to cloud storage and restore a cluster from that backup data.
--- hz-operator/step1/background.sh
+++ hz-operator/step1/background.sh
@@ -10,3 +10,31 @@ done
 EOF
 
 chmod +x password_prompt.sh
+
+CLUSTER_NAME=$(kubectl config view --minify -o jsonpath='{.clusters[0].name}')
+
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.5/config/manifests/metallb-native.yaml
+kubectl wait --for=condition=ready --timeout=60s -n metallb-system pod -l app=metallb
+HOST_MIN=$(docker network inspect -f '{{json .IPAM.Config}}' $CLUSTER_NAME | jq -r '.[1].Subnet' |  sed  -n 's/.0.0.16/.255.200/p')
+HOST_MAX=$(docker network inspect -f '{{json .IPAM.Config}}' $CLUSTER_NAME | jq -r '.[1].Subnet' |  sed  -n 's/.0.0.16/.255.250/p')
+IP_RANGE=$HOST_MIN-$HOST_MAX
+
+cat <<EOF | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: kind-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - $IP_RANGE
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: l2
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - kind-pool
+EOF
